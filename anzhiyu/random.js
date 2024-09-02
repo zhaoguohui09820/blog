17369e609e964894c537c2cd37b7acc2a6d73831
zhaoguohui09820 <zhaolixiangguohui@outlook.com>
2024-09-02 22:15:48 +0800

    Site updated: 2024-09-02 22:15:45

---
+++ anzhiyu/random.js
@@ -0,0 +1,56 @@
+var posts=["2024/08/29/关于班级的文件/","2024/07/29/加入我的Minecraft 服务器/","2024/08/28/本站音乐来源详解/","2024/07/18/“仙人”的画作/","2024/08/13/行走在云端，感受日落的美/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };var friend_link_list=[{"name":"anzhiyu主题","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg","color":"vip","tag":"技术"},{"name":"Akilarの糖果屋","link":"https://akilar.top/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/09/02/6311fc9de6507.webp","descr":"期待您的光临！","siteshot":"https://img02.anheyu.com/adminuploads/1/2022/09/02/6311fc39c5966.webp","color":"vip","tag":"生活"},{"name":"张洪Heo","link":"https://blog.zhheo.com/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/09/02/6311fc38f1465.webp","descr":"分享设计与科技生活","siteshot":"https://img02.anheyu.com/adminuploads/1/2022/09/02/6311fc3959f82.webp","color":"speed","tag":"生活"},{"name":"Leonus","link":"https://blog.leonus.cn/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/09/16/6324505c9890a.jpeg","descr":"进一寸有进一寸的欢喜。","siteshot":"https://img02.anheyu.com/adminuploads/1/2022/09/16/6324505c98fae.jpg","color":null,"tag":"技术"},{"name":"山岳库博","link":"https://kmar.top/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/10/06/633e9c4c2786f.png","descr":"开发学习启发性二刺螈","siteshot":"https://img02.anheyu.com/adminuploads/1/2022/10/06/633e9c4c3460b.jpg","color":null,"tag":"技术"},{"name":"Tianli","link":"https://tianli-blog.club","avatar":"https://img02.anheyu.com/adminuploads/1/2022/11/11/636db0d451fd0.webp","descr":"惟其不可能，所以才相信。","siteshot":"https://img02.anheyu.com/adminuploads/1/2022/11/11/636db0d451fd0.webp","color":null,"tag":"技术"},{"name":"Ariasaka","link":"https://yisous.xyz","avatar":"https://img02.anheyu.com/adminuploads/1/2022/12/11/6395bcc946fc9.png","descr":"人有悲欢离合 月有阴晴圆缺","siteshot":"https://img02.anheyu.com/adminuploads/1/2022/12/11/6395bcc1502e5.png","color":null,"tag":"技术"},{"name":"风记星辰","link":"https://www.thyuu.com/","avatar":"https://img02.anheyu.com/adminuploads/1/2023/07/04/64a3710cd824a.webp","descr":"简述热爱你来过的每一份温度","siteshot":"https://img02.anheyu.com/adminuploads/1/2023/07/04/64a371403e548.webp","color":null,"tag":"技术"},{"name":"Black Flies","link":"https://www.yyyzyyyz.cn/","avatar":"https://img02.anheyu.com/adminuploads/1/2023/07/14/64b0b00b33d75.webp","descr":"When nothing goes right,just go left.","siteshot":"https://img02.anheyu.com/adminuploads/1/2023/07/09/64aa91e87d4c6.webp","color":null,"tag":"技术"},{"name":"张时贰","link":"https://zhsher.cn/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/11/12/636ef2c1632a5.webp","descr":"环转码，爱敲代码的小张！","siteshot":"https://img02.anheyu.com/adminuploads/1/2023/02/03/63dc8d606e61a.webp","color":null,"tag":"技术"},{"name":"小孙同学","link":"https://blog.sunguoqi.com","avatar":"https://img02.anheyu.com/adminuploads/1/2023/07/21/64ba0d3e56247.webp","descr":"路虽远行则将至，事虽难做则必成！","siteshot":"https://img02.anheyu.com/adminuploads/1/2023/07/21/64ba0d149b0b3.webp","color":null,"tag":"技术"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","recommend":true},{"name":"無名小栈","link":"https://blog.imsyy.top/","avatar":"https://img.picui.cn/free/2024/08/25/66cb461d834db.ico","descr":"分享技术与科技生活","recommend":true},{"name":"杜老师说","link":"https://dusays.com/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/12/21/63a2acb9c07d0.png!linkavatar","descr":"师者，传道，授业，解惑！","recommend":false},{"name":"Fomalhaut🥝","link":"https://www.fomal.cc/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/09/02/6311ff42df52e.webp!linkavatar","descr":"Welcome to Fomalhaut🥝のTiny Home","recommend":true},{"name":"Mycpen","link":"https://blog.cpen.top/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/12/03/638b0c930633c.png!linkavatar","descr":"这是一个有趣的博客","recommend":false},{"name":"Ethan.Tzy","link":"https://fe32.top/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/09/17/6325338e1ad8a.jpg!linkavatar","descr":"古今之成大事者，不惟有超世之才，亦必有坚忍不拔之志","recommend":false},{"name":"LanYun","link":"https://lanyundev.com/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/10/04/633b93d34c802.jpg!linkavatar","descr":"Share Technology.","recommend":false},{"name":"一生雾梦の博客","link":"https://yswm-m.github.io/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/10/05/633d9b6d5fdc8.jpg!linkavatar","descr":"若一心追逐北极星,所经路线便能成为地图","recommend":false},{"name":"九九九感冒绫","link":"https://www.miku-39.cn/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/10/10/6343ab3ea1cd3.jpg!linkavatar","descr":"这是我的梦想,我会慢慢的完成它","recommend":false},{"name":"Jermyn","link":"https://www.jermyn.cn/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/10/15/634a2200ba851.jpg!linkavatar","descr":"To be, or not to be, that is the Question.","recommend":false},{"name":"ichika","link":"https://ichika.cc/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/10/15/634a2208a0879.jpg!linkavatar","descr":"Hello,gamer!","recommend":false},{"name":"心流","link":"https://blog.aqcoder.cn/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/10/18/634df5b5282f2.webp!linkavatar","descr":"分享知识，认识世界","recommend":false},{"name":"Echo","link":"https://www.liveout.cn/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/10/21/6351e69b70281.png!linkavatar","descr":"韶华不为少年留 恨悠悠 几时休","recommend":false}];
+    var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 3) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
